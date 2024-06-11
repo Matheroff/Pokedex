2e8af2bc8c291c7673b00b47abb3d89b0679c838
MATHIEU BUNIAZET <mathieu.buniazet@hotmail.fr>
2024-06-11 16:39:11 +0200

    PropTypes ajoutées avec succès !

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
-    name: "mew",
+    name: "Mew",
   },
 ];
 
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 function PokemonCard({pokemon}) {
 
     return (
@@ -11,5 +13,12 @@ function PokemonCard({pokemon}) {
         </figure>
       );
     }
+
+    PokemonCard.propTypes = {
+        pokemon: PropTypes.shape({
+          name: PropTypes.string.isRequired,
+          imgSrc: PropTypes.string,
+        }).isRequired
+      }
   
   export default PokemonCard;
